9a50b3f683dd218ba7f4a7e28bfe0da1516d5cea
Svend Christian Svendsen <svendcsvendsen@gmail.com>
2018-07-24 11:18:10 +0200

    Setup additional vim bindings.

--- home/.zsh/shell.zsh
+++ home/.zsh/shell.zsh
@@ -7,6 +7,17 @@ setopt extended_glob
 bindkey -v
 export KEYTIMEOUT=1
 
+# Use vim cli mode
+bindkey '^P' up-line-or-search
+bindkey '^N' down-line-or-search
+
+# backspace and ^h working even after returning from command mode
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+
+# ctrl-r starts searching history backward
+bindkey '^r' history-incremental-search-backward
+
 # Vim as default editor
 export EDITOR=nvim
 
