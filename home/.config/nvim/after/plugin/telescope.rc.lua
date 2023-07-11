d452503e6503744b8004b982f4542b6d10d1572d
Svend Christian Svendsen <svendcs@svendcs.com>
2023-07-11 19:52:16 +0200

    Update vim bindings

--- home/.config/nvim/after/plugin/telescope.rc.lua
+++ home/.config/nvim/after/plugin/telescope.rc.lua
@@ -13,6 +13,6 @@ telescope.setup({
 
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set("n", "<leader>p", ":Telescope find_files<CR>", opts)
+vim.keymap.set("n", "<leader>i", ":Telescope find_files<CR>", opts)
 vim.keymap.set("n", "<leader>o", ":Telescope lsp_dynamic_workspace_symbols<CR>", opts)
 vim.keymap.set("n", "gr", ":Telescope lsp_references<CR>", opts)
